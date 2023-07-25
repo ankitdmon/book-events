153ae855afaef030d3421ff1ded23dbb85d76e45
Ankit Mishra <93345314+ankitdmon@users.noreply.github.com>
2023-07-25 23:15:08 +0530

    First graphQl API

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-
+const { graphqlHTTP } = require('express-graphql'); // Fix the import statement here
+const { buildSchema } = require('graphql');
 const PORT = process.env.PORT || 5001;
-const app = express();
 
-app.get("/", (req, res )=> {
-    res.send("<h1>Hello GraphQL!!</h1>")
-})
+const app = express();
+app.use(bodyParser.json());
+
+app.use(
+  '/api',
+  graphqlHTTP({
+    // this will be single endpoint for the whole application as a REST API endpoint
+    schema: buildSchema(`
+      type rootQuery {
+        events: [String!]
+      }
+      type rootMutation {
+        createEvent(name: String): String
+      }
+      schema {
+        query: rootQuery
+        mutation: rootMutation
+      }
+    `),
+    rootValue: {
+      // after query(to fetch data), mutator (to update or delete data), resolvers
+      events: () => {
+        return ['Romantic Cooking', 'Coding', 'Dancing'];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
